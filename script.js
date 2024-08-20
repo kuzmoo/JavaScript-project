5bb43163abfa7c26f70956ad4e879c8850546fac
kuzmoo <nikola.kuz00@gmail.com>
2024-08-20 19:39:25 +0200

    komit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Elementi
+const dodaj = document.getElementById("dodajB");
+const unos = document.getElementById("unosB");
+const lista = document.getElementById("listaB");
+
+//Funckije
+
+const dodajBiljesku () {
+    const biljeska = unos.ariaValueMax.trim()
+
+    if (biljeska) {
+        create
+    }
+} 
+
+function (biljeska) {
+    
+} 
